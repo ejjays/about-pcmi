a82c5544bfd6026344bf02463212cc0a352fa6a0
ejjays <christsonalloso021@gmail.com>
2024-11-09 21:28:57 +0800

    Update Clients.jsx

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -1,5 +1,11 @@
 import { clientReviews } from '../constants/index.js';
 
+const handleClickWithDelay = (url) => {
+  setTimeout(() => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }, 1000); // Profile Click Delay
+};
+
 const Clients = () => {
   return (
     <section className="c-space mt-10 mb-0">
@@ -17,26 +23,42 @@ const Clients = () => {
 
               <div className="client-content">
                 <div className="flex gap-3">
-                  <img src={item.img} alt="reviewer" className="w-12 h-12 rounded-full" />
+                  <img
+                    src={item.img}
+                    alt="reviewer"
+                    className="w-12 h-12 rounded-full"
+                    onClick={() => handleClickWithDelay(item.profileLink)}
+                    style={{ cursor: 'pointer' }}
+                  />
                   <div className="flex flex-col">
-                    <p className="font-semibold text-white-800 flex items-center client-name">
-  {item.name}
-
- {/* <img src="/svg/verified.webp" */}
-
-<img src="/svg/Verifiedcheckmark.svg"
-
- alt="Verified" className="checkmark" />
-</p>
-                    <p className="text-white-500 md:text-base text-sm font-light">{item.position}</p>
+                    <p
+                      className="font-semibold text-white-800 flex items-center client-name"
+                      onClick={() => handleClickWithDelay(item.profileLink)}
+                      style={{ cursor: 'pointer' }}
+                    >
+                      {item.name}
+                      <img
+                        src="/svg/Verifiedcheckmark.svg"
+                        alt="Verified"
+                        className="checkmark"
+                      />
+                    </p>
+                    <p className="text-white-500 md:text-base text-sm font-light">
+                      {item.position}
+                    </p>
                   </div>
                 </div>
 
-               <div className="flex self-end items-center gap-2">
+                <div className="flex self-end items-center gap-2">
                   {Array.from({ length: 5 }).map((_, index) => (
-                    <img key={index} src="/assets/star.png" alt="star" className="w-5 h-5" />
+                    <img
+                      key={index}
+                      src="/assets/star.png"
+                      alt="star"
+                      className="w-5 h-5"
+                    />
                   ))}
-                </div> 
+                </div>
               </div>
             </div>
           </div>
