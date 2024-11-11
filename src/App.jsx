b3e0bb22287e86757a8655e88462be9edb822e9d
ejjays <christsonalloso021@gmail.com>
2024-11-11 20:29:10 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
@@ -5,17 +6,19 @@ import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
 import Projects from './sections/Projects.jsx';
 import WorkExperience from './sections/Experience.jsx';
+import Chatbot from './Chatbot.jsx';
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto relative">
       <Navbar />
       <Hero />
-      <About /> {/* This includes the Clients component internally */}
+      <About />
       <Projects />
       <WorkExperience />
       <Contact />
       <Footer />
+      <Chatbot /> {/* Add the Chatbot component here */}
     </main>
   );
 };
